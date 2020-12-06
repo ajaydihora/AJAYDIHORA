175e874acf6025f635aa4f5709462d0d9740b103
ajaydihora <68472521+ajaydihora@users.noreply.github.com>
2020-12-06 21:57:54 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Input, Button, Image, Hr, Icon, Text, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Formspree, Override, Menu, Section } from "@quarkly/components";
+import { MdFace } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Formspree errorMessage="Something went wrong" completeText="Success">
+			<Input name="email" placeholder="Email" margin-right="4px" />
+			<Button>
+				Submit
+			</Button>
+		</Formspree>
+		<Menu>
+			<Override slot="link-404">
+				404
+			</Override>
+		</Menu>
+		<Section />
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Image width="64px" height="64px" />
+			<Hr />
+			<Button>
+				Button
+			</Button>
+			<Icon category="md" icon={MdFace} background="#2a7438" />
+			<Image width="64px" height="64px" />
+			<Image width="64px" height="64px" />
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Hey I’m AJ DIHORA
+				</Text>
+				<Text font="--base">
+					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+				</Text>
+			</Box>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcd0500d84ef0001e947e32",
+				"5fcd0500d84ef0001e947e34"
+			]
+		},
+		"5fcd0500d84ef0001e947e32": {
+			"id": "5fcd0500d84ef0001e947e32",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcd0500d84ef0001e947e34": {
+			"id": "5fcd0500d84ef0001e947e34",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
